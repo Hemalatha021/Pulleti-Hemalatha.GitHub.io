39c8d17cac08f06310ba108def6adbd329817cf1
Hemalatha021 <104346911+Hemalatha021@users.noreply.github.com>
2022-06-04 21:05:27 +0530

    Add files via upload

---
+++ pweb/script.js
@@ -0,0 +1,9 @@
+document.getElementById("hrp").onclick=function(){
+ location.href="https://www.hackerrank.com/20L31A05J3";
+};
+document.getElementById("hep").onclick=function(){
+    location.href="https://www.hackerearth.com/@hemalatha324 ";
+};
+document.getElementById("ccp").onclick=function(){
+    location.href="https://www.codechef.com/users/hemalatha0210";
+}
